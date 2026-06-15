5b551911e54e3c9943d31a64d8d04a985e64b15c
niles <nileshmkjee123@gmail.com>
2026-06-15 19:51:30 +0530

    logo component

---
+++ client/src/components/logo.tsx
@@ -0,0 +1,24 @@
+import { PROTECTED_ROUTES } from "@/routes/routes"
+import { Link } from "react-router-dom"
+import { FlaskConical, ShirtIcon } from "lucide-react"
+import { cn } from "@/lib/utils"
+
+const Logo = ({ isCollapsed = false, className }: { isCollapsed?: boolean, className?: string }) => {
+    return (
+        <Link
+            to={PROTECTED_ROUTES.HOME}
+            className={cn("flex items-center gap-2 group transition-all shrink-0 select-none", className)}>
+            <div className={`flex items-center justify-center ${isCollapsed?"w-12 h-6":"w-8 h-8"} rounded-xl bg-linear-to-tr from-primary to-primary/80 text-primary-foreground shadow-sm group-hover:shadow-md group-hover:scale-120 transition-all duration-300`}>
+                <FlaskConical className="w-4 h-4 shrink-0" />
+            </div>
+            <span className={cn(`text-xl font-bold tracking-tight text-foreground`,
+                isCollapsed ? "hidden" : "block",
+                className
+            )}>
+                ApparelLab
+            </span>
+        </Link>
+    )
+}
+
+export default Logo
